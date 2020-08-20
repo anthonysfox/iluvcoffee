28f97c31e5b529ad09925a1c13b7297271400d3a
Anthony Fox <anthonysfox1@gmail.com>
2020-08-19 22:06:12 -0400

    Added in middleware to log request-response time

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -12,4 +18,15 @@ import { ConfigModule } from '@nestjs/config';
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Below are all example of how you could apply the middleware
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+    // consumer.apply(LoggingMiddleware).forRoutes('coffees');
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .forRoutes({ path: 'coffees', method: RequestMethod.GET });
+    // To exclude a route you can do
+    // consumer.apply(LoggingMiddleware).exclude('coffees').forRoutes('*')
+  }
+}
