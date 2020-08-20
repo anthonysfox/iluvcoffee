6e0a7bfd64afdcefcae85d736fa84e3b298653e3
Anthony Fox <anthonysfox1@gmail.com>
2020-08-19 21:55:20 -0400

    Added in a custom pipe that parses ints and throws an error in the returned data

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -16,6 +16,7 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -28,7 +29,8 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log(id);
     return this.coffeesService.findOne('' + id);
   }
 
